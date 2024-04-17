3310085490467b4813e5cb962e5844cc26079fc7
Aditya Anurag <aditya.anurag@nagarro.com>
2024-04-18 02:02:36 +0530

    First Commit

---
+++ test.js
@@ -0,0 +1,72 @@
+
+var x = 10;
+
+console.log("Hello, world!");
+
+var array = [1, 2, 3,];
+
+
+
+var a = 1, b = 2;
+
+// Excessive comments
+// Comment 1
+// Comment 2
+// Comment 3
+// Comment 4
+
+var unusedVariable = 20;
+
+var result = x > 0 ? "Positive" : (x < 0 ? "Negative" : "Zero");
+
+for (var i = 0; i < 10; i++) {
+  setTimeout(function() {
+    console.log(i);
+  }, 1000);
+}
+
+if (x == 10) {
+  console.log("x is equal to 10");
+}
+
+var request = new XMLHttpRequest();
+
+var numbers = [1, 2, 3];
+for (var i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+
+var square = function(x) {
+  return x * x;
+};
+
+
+var sum = new Function('a', 'b', 'return a + b');
+
+
+setInterval(function() {
+  console.log("Interval");
+}, 1000);
+
+
+var [first, second, third] = numbers;
+
+
+var value = 10 + "5";
+
+
+function sumAll(...args) {
+  return args.reduce((acc, val) => acc + val, 0);
+}
+
+debugger;
+
+var resultEval = eval("2 + 2");
+
+
+var longLine = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus nunc eget semper dictum. Duis non ante eu erat consequat posuere.";
+
+
+document.getElementById("exam         ple").style.color = "red";
+
